fc8b67cddd1c8a727ec34011c5087fe686e8a852
onghuilam <onghuilam@gmail.com>
2026-08-17 13:10:37 +0800

    corrected unit test

--- src/main/java/hl/llm/skills4j/ollama/OllamaSkillsMgr.java
+++ src/main/java/hl/llm/skills4j/ollama/OllamaSkillsMgr.java
@@ -360,10 +360,10 @@ public class OllamaSkillsMgr {
 		skillsMgr.setSilentMode(true);
 		skillsMgr.setDebugMode(true);
 		
-		Skill skill = skillsMgr.getOllamaSkill("pdf-to-text");
+		Skill skill = skillsMgr.getOllamaSkill("hello");
 		if(skill!=null)
 		{
-			String userprompt = "Describe this document.";
+			String userprompt = "Explain what is '"+skill.getSkillName()+"' in 1 sentence.";
 			System.out.println();
 			System.out.println(skillsMgr.execute(skill, new LLMReqInput(userprompt)));
 		}
